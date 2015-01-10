001b134853cb1d9c8aff8d4c8ab64ac141b6ae65
pandafulmanda <pandafulmanda@gmail.com>
2015-01-10 03:32:00 -0600

    separated out main file into modular parts, and added a way to try moves

---
+++ assets/components/block.js
@@ -0,0 +1,100 @@
+var Block = (function(window, undefined){
+
+  function Block(bitX, bitY, blockShape){
+
+    // default block shape to maze bit 
+    var blockShape = blockShape || [[1]];
+
+    this.originBit = {
+      x : bitX,
+      y: bitY
+    };
+
+    this.shape = blockShape;
+
+    this.bits = getBitAddressesForShape(this.originBit, this.shape);
+  }
+
+
+  Block.prototype.moveTo = function(toOriginX, toOriginY){
+
+    this.originBit.x = toOriginX;
+    this.originBit.y = toOriginY;
+
+    this.updateShape();
+  }
+
+  Block.prototype.updateShape = function(blockShape){
+    this.shape = blockShape || this.shape;
+
+    // update covering list of x's and y's as necessary.
+    this.bits = getBitAddressesForShape(this.originBit, this.shape);
+
+  }
+
+  Block.prototype.completeMove = function(direction){
+
+    this.originBit = moveBit(this.originBit, direction);
+
+    // update covering list of x's and y's as necessary.
+    this.bits = getBitAddressesForShape(this.originBit, this.shape);    
+  };
+
+  Block.prototype.tryMove = function(direction){
+    // could replace this cumbersome thing with vector multiplication later on.
+    // return new possible coordinates without updating this block.
+
+    return _.map(this.bits, function(bit){
+      return moveBit(bit, direction);
+    });
+  }
+
+  return Block;
+
+
+  // will have to store what bits the shape is covering as well.
+
+
+  function moveBit(bit, direction){
+
+    var moved = {
+      up : {
+        x : bit.x,
+        y : bit.y - 1
+      },
+      down : {
+        x : bit.x,
+        y : bit.y + 1
+      },
+      right : {
+        x : bit.x + 1,
+        y : bit.y
+      },
+      left : {
+        x : bit.x - 1,
+        y : bit.y
+      }
+    };
+
+    return moved[direction];
+  }
+
+
+  function getBitAddressesForShape(origin, shapeState){
+
+    var addresses = [];
+
+    readMatrix(shapeState, setBitsToShape)
+
+    return addresses;
+
+    function setBitsToShape(bitX, bitY, bit){
+      if(bit){
+        addresses.push({x: bitX + origin.x, y: bitY + origin.y, value: bit});        
+      }
+    }
+
+  }
+
+  
+})(window);

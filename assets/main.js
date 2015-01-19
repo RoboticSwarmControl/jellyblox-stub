a6b79050d536f93f3c585aa2fef952026a49259f
pandafulmanda <pandafulmanda@gmail.com>
2015-01-18 21:53:50 -0600

    update with working game logic, basic bootstrap style, and the 'ractive' library for showing up-to-date lists of bit locations

--- assets/main.js
+++ assets/main.js
@@ -3,11 +3,12 @@
   var game = new BoardState(maze.generate()),
     board = new Board('board', game.width(), game.height(), 60),
     mazeBlock = new Block(0, 0, game.state),
+
     // new blocks will eventually need to check for exiting obstacles as well
-    // startBlock = new Block(3, 6, [[2]]),
-    startBlock = new Block(3, 6, [[2]]),
-    anotherBlock = new Block(4, 6, [[3,3],[3, 0]]),
-    block3 = new Block(1, 1, [[2,2],[3, 0]]),
+    startBlock = new Block(7, 1, [[2]]),
+    anotherBlock = new Block(8, 1, [[3,3],[3, 0]]),
+    block3 = new Block(5, 1, [[2,2],[3, 0]]),
+
     allBlocks = [mazeBlock, startBlock, anotherBlock, block3],
     moveableBlocks = [startBlock, anotherBlock, block3],
     directions = {
@@ -15,7 +16,9 @@
       '38' : 'up',
       '39' : 'right',
       '40' : 'down'
-    };
+    },
+
+    blocksList, blockedBitsList;
 
   board.drawBoard(game.state);
 
@@ -26,12 +29,29 @@
   board.drawBoard(game.state);
 
 
+  blocksList = new Ractive({
+    el : document.getElementById('bits-list'),
+    template: document.getElementById('block-info').innerHTML,
+    data: {
+      blocks : moveableBlocks
+    }
+  });
+
+  blockedBitsList = new Ractive({
+    el : document.getElementById('blocked-bits-list'),
+    template: document.getElementById('blocked-bits-info').innerHTML,
+    data: {
+      bits : []
+    }
+  });
+
   document.addEventListener('keydown', function(keyEvent){
 
     var direction = directions[keyEvent.which],
       blocksToMove = [],
       blocksNewBits = [],
-      blockedBits = [];
+      blockedBits = [],
+      blockedBitsIter = 0;
 
     if(!direction){
       return;
@@ -40,12 +60,11 @@
     _.each(moveableBlocks, function(moveable, iter){
 
       // initial moving of blocks that can move without overlapping obstacle
-      var checkBlockBits = moveable.tryMove(direction);
-      var otherBlockBits = _(allBlocks).without(moveable).pluck('bits').flatten().value();
-
-      var overlapBits = _(checkBlockBits).map(function(checkBit){
-        return _.find(otherBlockBits, checkBit);
-      }).compact().value();
+      var checkBlockBits = moveable.tryMove(direction),
+        otherBlockBits = _(allBlocks).without(moveable).pluck('bits').flatten().value(),
+        overlapBits = _(checkBlockBits).map(function(checkBit){
+          return _.find(otherBlockBits, checkBit);
+        }).compact().value();
 
 
       if(_.isEmpty(overlapBits) || !_.find(overlapBits, {value: 1})){
@@ -63,36 +82,36 @@
 
     });
 
-
     if(blockedBits.length){
 
       // Should any of the blocksToMove not move?
 
       blockedBits = _(blockedBits).uniq().flatten().value();
-      // TODO
-      // may need to switch this each out for a while loop
-      // checking blocked bits
-      _.each(blocksNewBits, function(blockNewBits, blockNumber){
 
-        _.each(blockNewBits, function(newBit){
+      while(blockedBits[blockedBitsIter]){
+
+        _.each(blocksNewBits, function(blockNewBits, blockNumber){
 
           // if you can find the newBit in the blockedBit,
-          if(_.find(blockedBits, newBit)){
+          if(_.find(blockNewBits, {x: blockedBits[blockedBitsIter].x, y: blockedBits[blockedBitsIter].y })){
+
+            // add the current location of the block's bits as blocked.
+            _.each(blocksToMove[blockNumber].bits, function(bit){
+              blockedBits.push(bit);
+            });
 
             // remove the block that will contain that newBit.
             blocksToMove.splice(blockNumber, 1);
             blocksNewBits.splice(blockNumber, 1);
 
-            // TODO: NEED TO check newly blocked bits
-            _.each(blocksToMove.bits, function(bit){
-              blockedBits.push(bit);
-            })
-
           }
-        });
-      });
 
+        });
+
+        blockedBitsIter ++;
+      }
     }
+
 
     // Unset from game state all blocks
     _.each(blocksToMove, function(moveable){
@@ -104,10 +123,14 @@
       updateBlockAndState(moveable, direction);
     });
 
+
     // update canvas with new game state
     board.clearBoard(game.state);
     board.drawBoard(game.state);
 
+    // update displayed block and bit coordinates in list
+    blocksList.set('blocks', moveableBlocks);
+    blockedBitsList.set('bits', blockedBits);
   });
 
 
